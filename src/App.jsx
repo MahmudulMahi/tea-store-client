c42075e03b86beea20ad011c63903639f965e9ce
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-01 20:03:12 +0600

    Add client review and also team photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import { useLoaderData } from 'react-router-dom'
+import TeaCard from './TeaCard'
 
 function App() {
   
+  const teas=useLoaderData()
 
   return (
     <>
     
-      <h1 className='text-7xl'>Vite + React</h1>
+      <h1 className='text-7xl'>Hot Tea {teas.length}</h1>
+      {
+        teas.map(tea => <TeaCard key={tea._id} tea={tea}></TeaCard>)
+      }
    
   
     </>
