57a2dfe4735adbebe25a5afda596480f84ce74df
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-12-03 22:18:51 +0600

    Add client review and also team photo

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -39,6 +39,7 @@ const Users = () => {
             <th>1</th>
             <td>{user.email}</td>
             <td>{user.createdAt}</td>
+            <td>{user.lastLoggedAt}</td>
             <td>
               <button onClick={()=> handelDelete(user._id)}>X</button>
             </td>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 const auth = getAuth(app);
 
@@ -18,10 +18,16 @@ const AuthProvider = ({children}) => {
     return createUserWithEmailAndPassword(auth, email, password)
   }
 
+  const signInUser=(email,password)=>{
+    setLoading(true)
+    return signInWithEmailAndPassword(auth,email,password)
+  }
+
   const userInfo={
     user,
     loading,
     createUser,
+    signInUser
   }
   return (
     <AuthContext.Provider value={userInfo}>
